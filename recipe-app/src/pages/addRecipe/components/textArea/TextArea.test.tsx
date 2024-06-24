6fab6c76b1d82e6093c10a5bbed1df88d2d6506c
Linka351 <linka351@gmail.com>
2024-06-24 21:38:05 +0200

    add: input and textArea tests

---
+++ recipe-app/src/pages/addRecipe/components/textArea/TextArea.test.tsx
@@ -0,0 +1,68 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TextArea from "./TextArea";
+import userEvent from "@testing-library/user-event";
+//import userEvent from "@testing-library/user-event";
+
+describe("TextArea component", () => {
+	test("renders with initial value provided via props", () => {
+		const onChange = jest.fn();
+		const initialValue = "Initial description";
+
+		render(
+			<TextArea
+				placeholder='Enter description'
+				name='description'
+				onChange={onChange}
+				value={initialValue}
+				touched={false}
+				errors=''
+			/>
+		);
+
+		const textareaElement = screen.getByPlaceholderText(
+			"Enter description"
+		) as HTMLTextAreaElement;
+		expect(textareaElement).toBeInTheDocument();
+		expect(textareaElement.value).toBe(initialValue);
+	});
+
+	test("displays error message when touched is true and errors are present", () => {
+		render(
+			<TextArea
+				placeholder='Enter description'
+				name='description'
+				onChange={jest.fn()}
+				value=''
+				touched={true}
+				errors='Description is required'
+			/>
+		);
+
+		const errorElement = screen.getByText("Description is required");
+		expect(errorElement).toBeInTheDocument();
+	});
+
+	test("should change textArea value", async () => {
+		const onChange = jest.fn();
+		render(
+			<TextArea
+				placeholder='Enter description'
+				name='description'
+				onChange={onChange}
+				value=''
+				touched={true}
+				errors=''
+			/>
+		);
+
+		const textAreaElement = screen.getByPlaceholderText("Enter description");
+		expect(textAreaElement).toHaveValue("");
+
+		await userEvent.type(textAreaElement, "test");
+		expect(onChange).toHaveBeenCalledTimes(4);
+		expect(textAreaElement).toHaveValue("test");
+
+		screen.debug();
+	});
+});
