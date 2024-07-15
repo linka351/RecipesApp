6f5df84a44e41d23ac2fd9fcf54848f03537d83c
Kamil Linka <49818530+linka351@users.noreply.github.com>
2024-07-15 13:47:38 +0200

    Navbar component (#10)
    * add: Navbar component
    
    * add: navbar tests
    
    * change: toggleMenu function
    
    * change: width

--- recipe-app/src/pages/landing/landingPage/LandingPage.tsx
+++ recipe-app/src/pages/landing/landingPage/LandingPage.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../../../components/navbar/Navbar";
+
 import Footer from "../../components/footer/Footer";
 
 function LandingPage() {
+	return <Navbar />;
 	return <Footer />;
 }
 
