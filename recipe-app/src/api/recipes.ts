7e95bf4b483f5c1f7aab1efd876ea5d82524cf81
Linka351 <linka351@gmail.com>
2024-08-07 20:14:02 +0200

    add: AddMealPlan component

--- recipe-app/src/api/recipes.ts
+++ recipe-app/src/api/recipes.ts
@@ -1,11 +1,22 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
 import { FormValues } from "../pages/app/recipes/add/addRecipe/AddRecipe";
 import { db } from "../firebase/firebaseConfig";
+import { Recipe } from "../types/editRecipe";
 
 const add = async (values: FormValues) => {
 	return addDoc(collection(db, "recipes"), values);
 };
 
+const getAll = async () => {
+	const q = query(collection(db, "recipes"));
+	const snapShot = await getDocs(q);
+	return snapShot.docs.map(doc => ({
+		id: doc.id,
+		...doc.data(),
+	})) as Recipe[];
+};
+
 export const recipeApi = {
 	add,
+	getAll,
 };
--- recipe-app/src/pages/app/mealPlans/add/addMealPlan/AddMealPlan.tsx
+++ recipe-app/src/pages/app/mealPlans/add/addMealPlan/AddMealPlan.tsx
@@ -1,5 +1,130 @@
+import { useEffect, useState } from "react";
+import { Recipe } from "../../../../../types/editRecipe";
+import { recipeApi } from "../../../../../api/recipes";
+
+import "./addMealPlan.scss";
+
 function AddMealPlan() {
-	return <p>Add Meal Plan</p>;
+	const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+	useEffect(() => {
+		async function fetchRecipes() {
+			const recipeList = await recipeApi.getAll();
+			setRecipes(recipeList);
+		}
+
+		fetchRecipes();
+	}, []);
+
+	const selectOption = () => {
+		return (
+			<select>
+				{recipes.map(recipe => (
+					<option key={recipe.id} value={recipe.id}>
+						{recipe.name}
+					</option>
+				))}
+			</select>
+		);
+	};
+
+	return (
+		<div className='container'>
+			<p className='title'>Nowy Plan</p>
+			<form className='add-meal-plan-form'>
+				<label className='label'>
+					Nazwa Planu
+					<input type='text' className='plan-input' />
+				</label>
+				<label className='label'>
+					Opis Planu
+					<textarea className='plan-input plan-input-description' />
+				</label>
+				<table>
+					<thead>
+						<tr className='table-header'>
+							<th></th>
+							<th>Śniadanie</th>
+							<th>Drugie Śniadanie</th>
+							<th>Zupa</th>
+							<th>Drugie danie</th>
+							<th>kolacja</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th className='table-header'>Poniedziałek</th>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+						</tr>
+					</tbody>
+					<tbody>
+						<tr>
+							<th className='table-header'>Wtorek</th>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+						</tr>
+					</tbody>
+					<tbody>
+						<tr>
+							<th className='table-header'>Środa</th>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+						</tr>
+					</tbody>
+					<tbody>
+						<tr>
+							<th className='table-header'>Czwartek</th>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+						</tr>
+					</tbody>
+					<tbody>
+						<tr>
+							<th className='table-header'>Piątek</th>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+						</tr>
+					</tbody>
+					<tbody>
+						<tr>
+							<th className='table-header'>Sobota</th>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+						</tr>
+					</tbody>
+					<tbody>
+						<tr>
+							<th className='table-header'>Niedziela</th>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+							<td>{selectOption()}</td>
+						</tr>
+					</tbody>
+				</table>
+			</form>
+		</div>
+	);
 }
 
 export default AddMealPlan;
