0476624bdd7ea25a8775746dff0eadc355055f94
Linka351 <linka351@gmail.com>
2024-10-09 21:49:04 +0200

    add: MealPlansForm component

--- recipe-app/src/api/recipes.ts
+++ recipe-app/src/api/recipes.ts
@@ -29,6 +29,13 @@ const getById = async (id: string) => {
 	return { id: documentSnapshot.id, ...documentSnapshot.data() } as Recipe;
 };
 
+const getMealPlanById = async (id: string) => {
+	const documentReference = doc(db, "mealPlans", id);
+	const documentSnapshot = await getDoc(documentReference);
+
+	return { id: documentSnapshot.id, ...documentSnapshot.data() } as WeeklyPlan;
+};
+
 const remove = async (id: string) => {
 	const documentReference = doc(db, "recipes", id);
 	await deleteDoc(documentReference);
@@ -43,8 +50,25 @@ const getAll = async () => {
 	})) as Recipe[];
 };
 
+const getAllWeeklyPlans = async () => {
+	const q = query(collection(db, "mealPlans"));
+	const snapShot = await getDocs(q);
+	return snapShot.docs.map(doc => ({
+		id: doc.id,
+		...doc.data(),
+	})) as WeeklyPlan[];
+};
+
 const addMealPlan = async (mealPlan: WeeklyPlan) => {
 	return addDoc(collection(db, "mealPlans"), mealPlan);
+};
+
+const updateMealPlan = async (
+	docId: string,
+	updatedData: Omit<WeeklyPlan, "id">
+) => {
+	const docRef = doc(db, "mealPlans", docId);
+	await updateDoc(docRef, updatedData);
 };
 
 export const recipeApi = {
@@ -54,4 +78,7 @@ export const recipeApi = {
 	getById,
 	remove,
 	addMealPlan,
+	updateMealPlan,
+	getMealPlanById,
+	getAllWeeklyPlans,
 };
