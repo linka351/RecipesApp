f3f665a030eaa6abdafac793e33d079618efcfe3
Linka351 <linka351@gmail.com>
2024-11-21 17:08:53 +0100

    add: upload image

--- recipe-app/src/firebase/firebaseConfig.ts
+++ recipe-app/src/firebase/firebaseConfig.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_RECIPES_APP_API_KEY,
@@ -13,6 +14,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
--- recipe-app/src/pages/app/recipes/recipesForm/RecipesForm.tsx
+++ recipe-app/src/pages/app/recipes/recipesForm/RecipesForm.tsx
@@ -1,12 +1,14 @@
-import { FormikHelpers, useFormik } from "formik";
-import InstructionsForm from "./components/instructionsForm/InstructionsForm";
-import IngredientsForm from "./components/ingredientsForm/IngredientsForm";
+// import { FormikHelpers, useFormik } from "formik";
+// import InstructionsForm from "./components/instructionsForm/InstructionsForm";
+// import IngredientsForm from "./components/ingredientsForm/IngredientsForm";
 import Input from "../../../../components/inputs/Input";
-import TextArea from "../../../../components/textAreas/TextArea";
-import { recipeApi } from "../../../../api/recipes";
-import { validationSchema } from "./RecipeForm.validation";
-
+// import TextArea from "../../../../components/textAreas/TextArea";
+// import { recipeApi } from "../../../../api/recipes";
+// import { validationSchema } from "./RecipeForm.validation";
 import "./recipesForm.scss";
+import { useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "../../../../firebase/firebaseConfig";
 
 export interface FormValues {
 	id?: string;
@@ -14,6 +16,7 @@ export interface FormValues {
 	description: string;
 	instructions: string[];
 	ingredients: string[];
+	image?: string;
 }
 
 interface RecipesFormProps {
@@ -21,79 +24,139 @@ interface RecipesFormProps {
 	onSubmit?: () => void;
 }
 
-function RecipesForm({ initialValues, onSubmit }: RecipesFormProps) {
-	const defaultValues: FormValues = {
-		name: "",
-		description: "",
-		instructions: [],
-		ingredients: [],
-	};
+function RecipesForm({}: RecipesFormProps) {
+	const [imageUpload, setImageUpload] = useState<File | null>(null);
 
-	const formik = useFormik<FormValues>({
-		initialValues: initialValues || defaultValues,
-		validationSchema,
-		onSubmit: handleSubmit,
-	});
-
-	async function handleSubmit(
-		values: FormValues,
-		formikHelpers: FormikHelpers<FormValues>
-	) {
-		try {
-			if (values.id) {
-				await recipeApi.update(values.id!, {
-					name: values.name,
-					description: values.description,
-					instructions: values.instructions,
-					ingredients: values.ingredients,
-				});
-			} else {
-				await recipeApi.add(values);
-			}
-			formikHelpers.resetForm();
-			if (onSubmit) onSubmit();
-		} catch (e: any) {
-			console.log({ e });
+	const uploadImage = async () => {
+		if (!imageUpload) {
+			alert("Najpierw wybierz zdjęcie!");
+			return;
 		}
-	}
 
-	const handleAddInstruction = (instruction: string): void => {
-		formik.setFieldValue("instructions", [
-			...formik.values.instructions,
-			instruction,
-		]);
-	};
+		const imageRef = ref(storage, `images/${imageUpload.name}`);
 
-	const handleAddIngredient = (ingredient: string): void => {
-		formik.setFieldValue("ingredients", [
-			...formik.values.ingredients,
-			ingredient,
-		]);
-	};
+		try {
+			await uploadBytes(imageRef, imageUpload);
 
-	const handleRemoveElement = (
-		index: number,
-		element: "instructions" | "ingredients"
-	): void => {
-		const updatedElements = [...formik.values[element]];
-		updatedElements.splice(index, 1);
-		formik.setFieldValue(element, updatedElements);
+			const downloadURL = await getDownloadURL(imageRef);
+
+			alert("Zdjęcie zostało dodane!");
+			console.log("Download URL:", downloadURL);
+		} catch (error) {
+			console.error("Błąd podczas przesyłania zdjęcia:", error);
+			alert("Wystąpił problem z przesłaniem zdjęcia. Spróbuj ponownie.");
+		}
 	};
 
-	const handleEditIngredient = (index: number, ingredient: string) => {
-		const updatedIngredients = [...formik.values.ingredients];
-		updatedIngredients[index] = ingredient;
-		formik.setFieldValue("ingredients", updatedIngredients);
-	};
-	const handleEditInstruction = (index: number, instruction: string) => {
-		const updatedInstructions = [...formik.values.instructions];
-		updatedInstructions[index] = instruction;
-		formik.setFieldValue("instructions", updatedInstructions);
-	};
+	// const defaultValues: FormValues = {
+	// 	name: "",
+	// 	description: "",
+	// 	instructions: [],
+	// 	ingredients: [],
+	// };
+
+	// const formik = useFormik<FormValues>({
+	// 	initialValues: initialValues || defaultValues,
+	// 	validationSchema,
+	// 	onSubmit: handleSubmit,
+	// });
+
+	// async function handleSubmit(
+	// 	values: FormValues,
+	// 	formikHelpers: FormikHelpers<FormValues>
+	// ) {
+	// 	try {
+	// 		if (values.id) {
+	// 			await recipeApi.update(values.id!, {
+	// 				name: values.name,
+	// 				description: values.description,
+	// 				instructions: values.instructions,
+	// 				ingredients: values.ingredients,
+	// 				image: imageUrl,
+	// 			});
+	// 		} else {
+	// 			await recipeApi.add(values);
+	// 		}
+	// 		formikHelpers.resetForm();
+	// 		if (onSubmit) onSubmit();
+	// 	} catch (e: any) {
+	// 		console.log({ e });
+	// 	}
+	// }
+
+	// const handleAddInstruction = (instruction: string): void => {
+	// 	formik.setFieldValue("instructions", [
+	// 		...formik.values.instructions,
+	// 		instruction,
+	// 	]);
+	// };
+
+	// const handleAddIngredient = (ingredient: string): void => {
+	// 	formik.setFieldValue("ingredients", [
+	// 		...formik.values.ingredients,
+	// 		ingredient,
+	// 	]);
+	// };
+
+	// const handleRemoveElement = (
+	// 	index: number,
+	// 	element: "instructions" | "ingredients"
+	// ): void => {
+	// 	const updatedElements = [...formik.values[element]];
+	// 	updatedElements.splice(index, 1);
+	// 	formik.setFieldValue(element, updatedElements);
+	// };
+
+	// const handleEditIngredient = (index: number, ingredient: string) => {
+	// 	const updatedIngredients = [...formik.values.ingredients];
+	// 	updatedIngredients[index] = ingredient;
+	// 	formik.setFieldValue("ingredients", updatedIngredients);
+	// };
+	// const handleEditInstruction = (index: number, instruction: string) => {
+	// 	const updatedInstructions = [...formik.values.instructions];
+	// 	updatedInstructions[index] = instruction;
+	// 	formik.setFieldValue("instructions", updatedInstructions);
+	// };
+
+	// const handleFileUpload = (
+	// 	event: React.ChangeEvent<HTMLInputElement>
+	// ): void => {
+	// 	const file = event.target.files?.[0];
+	// 	if (file) {
+	// 		const reader = new FileReader();
+
+	// 		reader.onload = (e: ProgressEvent<FileReader>) => {
+	// 			if (e.target?.result) {
+	// 				setImageUrl(e.target.result as string);
+	// 			}
+	// 		};
+
+	// 		reader.readAsDataURL(file);
+	// 	}
+	// };
 
 	return (
 		<div className='recipe-container'>
-			<div className='recipe-name'>
+			<Input
+				type='file'
+				name='image'
+				onChange={e => {
+					const file = e.target.files?.[0];
+					if (file) {
+						setImageUpload(file);
+						console.log("Wybrano plik:", file.name);
+					}
+				}}
+			/>
+			<button onClick={uploadImage}>Upload</button>
+			{/* {imageUrl && (
+				<img
+					style={{ height: "200px", width: "200px" }}
+					src={imageUrl}
+					alt='Uploaded preview'
+				/>
+			)} */}
+			{/* <div className='recipe-name'>
 				<h2>{initialValues?.id ? "Edytuj Przepis" : "Nowy Przepis"}</h2>
 
 				<button
@@ -134,7 +197,7 @@ function RecipesForm({ initialValues, onSubmit }: RecipesFormProps) {
 					touched={!!formik.touched.ingredients}
 					errors={formik.errors.ingredients || ""}
 				/>
-			</div>
+			</div> */}
 		</div>
 	);
 }
