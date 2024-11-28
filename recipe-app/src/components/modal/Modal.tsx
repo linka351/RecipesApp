b1ea6bcc075704bbd2e6d801d4a1a37621aa3d05
mkokosinski <mkokosinski91@gmail.com>
2024-11-28 17:51:53 +0100

    code

--- recipe-app/src/components/modal/Modal.tsx
+++ recipe-app/src/components/modal/Modal.tsx
@@ -1,27 +1,30 @@
 import { ReactNode } from "react";
+import { createPortal } from "react-dom";
 
+const portals = document.getElementById("portals")!;
 
 type ModalProps = {
-	isOpen: boolean;
-	onClose: () => void;
-	children: ReactNode;
-	text?: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+  text?: string;
 };
 
 const Modal = ({ isOpen, onClose, children, text }: ModalProps) => {
-	if (!isOpen) return null;
+  if (!isOpen) return null;
 
-	return (
-		<div className='modal'>
-			<div className='modal-content'>
-				{text && <h2 className='modal-text'>{text}</h2>}
-				<button onClick={onClose} className='modal-close'>
-					X
-				</button>
-				<div className='modal-body'>{children}</div>
-			</div>
-		</div>
-	);
+  return createPortal(
+    <div className="modal">
+      <div className="modal-content">
+        {text && <h2 className="modal-text">{text}</h2>}
+        <button onClick={onClose} className="modal-close">
+          X
+        </button>
+        <div className="modal-body">{children}</div>
+      </div>
+    </div>,
+    portals
+  );
 };
 
 export default Modal;
