f5315fd01409d9290eb725eb1e0f132fd27ec776
Linka351 <linka351@gmail.com>
2025-06-02 19:26:42 +0200

    add: types to role and status

--- recipe-app/src/constants/status.const.ts
+++ recipe-app/src/constants/status.const.ts
@@ -1,4 +1,6 @@
-export const STATUS = {
+import { Status } from "../types/status";
+
+export const STATUS: Record<string, Status> = {
 	PUBLIC: "public",
 	PRIVATE: "private",
 } as const;
--- recipe-app/src/types/user.ts
+++ recipe-app/src/types/user.ts
@@ -1,5 +1,7 @@
+import { Role } from "./role";
+
 export type User = {
 	id: string;
 	email: string;
-	role: "user" | "admin";
+	role: Role;
 };
