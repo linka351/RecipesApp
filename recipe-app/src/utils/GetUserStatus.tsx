2ab3347b245f918a23a9abedc7dfa8628df4fc4b
Linka351 <linka351@gmail.com>
2025-07-28 18:12:44 +0200

    refactor: getUserStatus name

--- recipe-app/src/utils/GetUserStatus.tsx
+++ recipe-app/src/utils/GetUserStatus.tsx
@@ -3,8 +3,8 @@ import { USER_ROLE } from "../constants/user.const";
 import { Role } from "../types/role";
 import { Status } from "../types/status";
 
-function GetUserStatus(role: Role): Status {
+function getUserStatus(role: Role): Status {
 	return role === USER_ROLE.ADMIN ? STATUS.PUBLIC : STATUS.PRIVATE;
 }
 
-export default GetUserStatus;
+export default getUserStatus;
